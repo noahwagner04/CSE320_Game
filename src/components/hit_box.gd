69c7e7c21d5cdb92b134c6f207372b36d80fb7cc
Noah Wagner <wagnernoah@icloud.com>
2024-01-30 23:20:42 -0800

    refactor code

--- src/components/hit_box.gd
+++ src/components/hit_box.gd
@@ -1,6 +1,20 @@
-extends Area2D
+class_name HitBox extends Area2D
 
-class_name HitBox
+@export var damage: float = 1
+@export var knockback: float = 0
 
-@export var damage : float = 1
-@export var knockback : float = 0
+enum ResponseType {NO_RESPONSE, DISABLE, TEMPORARY_DISABLE}
+@export var response: ResponseType = ResponseType.NO_RESPONSE
+@export_range(0.001, 60, 0.001, "or_greater", "suffix:s") 
+var disable_time: float = 1
+
+func _on_area_entered(_area):
+	match response:
+		ResponseType.DISABLE:
+			set_deferred("monitoring", false)
+		ResponseType.TEMPORARY_DISABLE:
+			set_deferred("monitoring", false)
+			await get_tree().create_timer(disable_time).timeout
+			set_deferred("monitoring", true)
+		_:
+			return
--- src/enemies/giant_vole.gd
+++ src/enemies/giant_vole.gd
@@ -1,16 +1,14 @@
 extends CharacterBody2D
 
-@export var speed : float = 100
-@export var agro_dist : float = 300
-@export var attack_cooldown : float = 1
-@export var health : float = 20
-@onready var player : Node = get_tree().get_first_node_in_group("player")
-@onready var collider : CollisionShape2D = %HitBoxShape
-var _target : Node2D
-var home : Node2D = Node2D.new()
-var rand_target_mod = Vector2((randf() * 2 - 1) * 10, (randf() * 2 - 1) * 10)
-var knockback : Vector2 = Vector2.ZERO
-var friction : float = 1000
+@export var speed: float = 100
+@export var agro_dist: float = 300
+@onready var player: Node = get_tree().get_first_node_in_group("player")
+@onready var collider: CollisionShape2D = %HitBoxShape
+var _target: Node2D
+var home: Node2D = Node2D.new()
+var rand_target_mod: Vector2 = Vector2((randf() * 2 - 1) * 10, (randf() * 2 - 1) * 10)
+var knockback: Vector2 = Vector2.ZERO
+var friction: float = 1000
 
 func _ready():
 	home.global_position = global_position
@@ -42,13 +40,8 @@ func _physics_process(_delta):
 	velocity += knockback
 	move_and_slide()
 
-func _on_hit_box_area_entered(_area):
-	collider.set_deferred("disabled", true)
-	await get_tree().create_timer(attack_cooldown).timeout
-	collider.set_deferred("disabled", false)
+func _on_health_container_health_depleted():
+	queue_free()
 
 func _on_hurt_box_hurt(hit_box):
-	health -= hit_box.damage
 	knockback = (global_position - hit_box.global_position).normalized() * hit_box.knockback
-	if health <= 0:
-		queue_free()
