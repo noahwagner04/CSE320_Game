fd9b5f3cfe3b55ce70fb6ee8354bba109cc53a29
micahwagner <micah.wagner@icloud.com>
2024-01-31 19:13:03 -0800

    add player connections

--- src/UI/main_menu.gd
+++ src/UI/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://src/main.tscn")
+	get_tree().change_scene_to_file("res://src/UI/connection_scene.tscn")
 
 
 func _on_settings_button_pressed():
