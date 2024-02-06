d5a512e6968da0d57fd4e3cdce5f60a778208622
micahwagner <micah.wagner@icloud.com>
2024-02-06 13:12:23 -0800

    sync player joins and disconnects

--- src/UI/connection_scene.gd
+++ src/UI/connection_scene.gd
@@ -1,23 +1,12 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_host_button_pressed():
 	ConnectionManager.set_peer_host()
-	ConnectionManager.player_name = $NameTextFeild.text	
-	get_tree().change_scene_to_file("res://src/main.tscn")
+	ConnectionManager.player_name = $NameTextFeild.text
+	visible = false
 
 
 func _on_join_button_pressed():
-	ConnectionManager.set_peer_client()	
+	ConnectionManager.set_peer_client()
 	ConnectionManager.player_name = $NameTextFeild.text
-	get_tree().change_scene_to_file("res://src/main.tscn")
+	visible = false
--- src/connection_manager.gd
+++ src/connection_manager.gd
@@ -12,10 +12,6 @@ func _ready():
 	multiplayer.connected_to_server.connect(connected_to_server)
 	multiplayer.connection_failed.connect(connection_failed)
 
-
-func _process(delta):
-	pass
-
 # gets called on both servers and clients
 func peer_connected(id):
 	print("Player connected " + str(id))
@@ -23,6 +19,7 @@ func peer_connected(id):
 # gets called on both servers and clients
 func peer_disconnected(id):
 	print("Player disconnected " + str(id))
+	sync_player_disconnect(id)
 	
 # gets called only on clients
 func connected_to_server():
@@ -42,6 +39,9 @@ func set_peer_host():
 		
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 	multiplayer.set_multiplayer_peer(peer)
+	
+	sync_player_join(player_name, multiplayer.get_unique_id())
+	
 	print("Waiting for players...")
 	
 func set_peer_client():
@@ -56,11 +56,36 @@ func sync_player_join(pname, id):
 	if !players.has(id):
 		players[id] = {
 			"name": pname,
-			"id": id
+			"id": id,
 		}
+		instantiate_player(id)
+		
 	
 	# if we're the server, tell all other players a new player joined
 	if multiplayer.is_server():
 		# rpc to all players to send the new player all other players
 		for i in players:
 			sync_player_join.rpc(players[i].name, i)
+			
+@rpc("any_peer")
+func sync_player_disconnect(id):
+	# add given player to player dictionary if entry doesn't exist
+	
+	players.erase(id)
+	delete_player(id)
+	# if we're the server, tell all other players a new player joined
+	if multiplayer.is_server():
+		sync_player_disconnect.rpc(id)
+
+func instantiate_player(id):
+	var new_player = preload("res://src/player/player.tscn").instantiate()
+	new_player.name = str(id)
+	new_player.global_position = Vector2(cos(randf()), sin(randf())) * 100
+	get_node("/root").add_child(new_player)
+	
+func delete_player(id):
+	var players = get_tree().get_nodes_in_group("player")
+	for i in players:
+		if i.name == str(id):
+			i.queue_free()
+
