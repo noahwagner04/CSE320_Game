52a8e41bfa010bd121513e941725f1493190c30c
Jambr0li <jasonkovalenko@gmail.com>
2024-02-08 18:27:22 -0800

    Created player_menu scene and created a load stats function - not dynamic yet

---
+++ src/UI/player_menu.gd
@@ -0,0 +1,40 @@
+extends Control
+
+var health_label : RichTextLabel
+var defense_label : RichTextLabel
+var hp_regen_label : RichTextLabel
+var attack_label : RichTextLabel
+var dexterity_label : RichTextLabel
+var speed_label : RichTextLabel
+var sp_regen_label : RichTextLabel
+var stamina_label : RichTextLabel
+
+# Called when the node enters the scene tree for the first time.
+
+func load_stats_data():
+	health_label = $VBoxContainer/Health
+	defense_label = $VBoxContainer/Defense
+	hp_regen_label = $"VBoxContainer/HP Regen"
+	attack_label = $VBoxContainer/Attack
+	dexterity_label = $VBoxContainer/Dexterity
+	speed_label = $VBoxContainer/Speed
+	sp_regen_label = $"VBoxContainer/SP Regen"
+	stamina_label = $VBoxContainer/Stamina
+	health_label.bbcode_text = "Health "
+	defense_label.bbcode_text = "Defense "
+	hp_regen_label.bbcode_text = "HP Regeneration "
+	attack_label.bbcode_text = "Attack "
+	dexterity_label.bbcode_text = "Dexterity "
+	speed_label.bbcode_text = "Speed "
+	sp_regen_label.bbcode_text = "SP Regeneration "
+	stamina_label.bbcode_text = "Stamina "
+	
+
+func _ready():
+	load_stats_data()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
