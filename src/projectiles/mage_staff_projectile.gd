cad827690a1cd1f752e82a69e0eb9d38e9002b7b
Noah Wagner <wagnernoah@icloud.com>
2024-02-09 12:35:58 -0800

    make style consistant, fix warnings, general cleanup

--- src/projectiles/mage_staff_projectile.gd
+++ src/projectiles/mage_staff_projectile.gd
@@ -1,27 +1,31 @@
 extends Node2D
 
 @export var projectile_direction: Vector2 = Vector2(0.0, 0.0)
-@export var projectile_speed: int = 100
-@export var projectile_damage: int = 10
+@export var projectile_speed: float = 100
+@export var projectile_damage: float = 10
 @export_range(5, 500, 1) var projectile_range: int = 50
 @export_enum("line", "swing") var projectile_type: String = "line"
 @export var aoe_explosion: bool = false
-var explosion: PackedScene = preload("res://src/projectiles/explosion.tscn")
 
+var explosion: PackedScene = preload("res://src/projectiles/explosion.tscn")
 var projectile_lifetime: float = projectile_speed / projectile_range
+
 
 func _ready():
 	$HitBox.damage = projectile_damage
 	await get_tree().create_timer(projectile_lifetime).timeout
 	queue_free()
 
+
 func _process(_delta):
 	position += projectile_direction * _delta * projectile_speed
+
 
 func explode():
 	if aoe_explosion == true:
 		var explosion_instance = explosion.instantiate()
 		add_child(explosion_instance)
 
-func _on_hit_box_area_entered(area):
+
+func _on_hit_box_area_entered(_area):
 	explode()
