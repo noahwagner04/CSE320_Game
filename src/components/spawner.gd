79a40000352d9167202d41024ff58943cfdc23d6
Noah Wagner <wagnernoah@icloud.com>
2024-02-08 18:26:20 -0800

    refactor style

--- src/components/spawner.gd
+++ src/components/spawner.gd
@@ -1,6 +1,6 @@
 @tool
-extends Marker2D
 class_name Spawner
+extends Marker2D
 
 @export_range(0.001, 60, 0.001, "or_greater", "suffix:s") 
 var frequency: float = 1
@@ -18,15 +18,30 @@ var radius: int = 32:
 	set(new_scene):
 		scene = new_scene
 		update_configuration_warnings()
+
 var spawned_count: int = 0
 var timer: Timer = Timer.new()
+
 
 func _ready():
 	timer.timeout.connect(spawn)
 	add_child(timer)
 
+
+func _draw():
+	if Engine.is_editor_hint() and debug:
+		draw_circle(Vector2.ZERO, radius, Color(1, 1, 0, 0.3))
+
+
+func _get_configuration_warnings():
+	if scene == null:
+		return ["Scene to spawn is not set!"]
+	elif not (scene.instantiate() is Node2D):
+		return ["Provided scene must extend Node2D for Spawner to work."]
+
+
 func spawn():
-	if spawned_count > max_spawn:
+	if spawned_count >= max_spawn:
 		return
 	var instance = scene.instantiate()
 	if instance is Node2D:
@@ -36,23 +51,16 @@ func spawn():
 	add_child(instance)
 	spawned_count += 1
 
+
 func start_spawning(new_frequency : float = frequency):
 	if new_frequency != frequency:
 		frequency = new_frequency
 	timer.start(frequency)
 
+
 func stop_spawning():
 	timer.stop()
 
+
 func _on_despawn():
 	spawned_count -= 1
-
-func _draw():
-	if Engine.is_editor_hint() and debug:
-		draw_circle(Vector2.ZERO, radius, Color(1, 1, 0, 0.3))
-
-func _get_configuration_warnings():
-	if scene == null:
-		return ["Scene to spawn is not set!"]
-	elif not (scene.instantiate() is Node2D):
-		return ["Provided scene must extend Node2D for Spawner to work."]
