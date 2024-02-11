fa46f428989a3fc17b552a03265cd78d34511ad3
Lukas-bit <lukasrenschler@gmail.com>
2024-02-10 19:15:29 -0800

    Create Larger Summon Ring On Condition
    If the behemoth vole is brought down to 30% or below its max hp before
    it spawns the first ring of voles, then a secondary larger ring of
    Giant Voles will spawn around the player in addition to the smaller
    first ring. Also, increased the damage behemoth can do by 5 points.

--- src/enemies/boss_behemoth_vole.gd
+++ src/enemies/boss_behemoth_vole.gd
@@ -2,34 +2,42 @@ extends CharacterBody2D
 
 @export var agro_dist: float = 300
 
-var _rand_target_mod := Vector2((randf() * 2 - 1) * 10, (randf() * 2 - 1) * 10)
-var _target: Node2D
+#var _rand_target_mod := Vector2((randf() * 2 - 1) * 10, (randf() * 2 - 1) * 10)
+var _target #: Node2D
 
-@onready var player: Node = get_tree().get_first_node_in_group("player")
-@onready var motion_controller: MotionController = %MotionController
 @onready var health_checker: HealthContainer = %HealthContainer
+@onready var motion_controller: MotionController = %MotionController
+@onready var player: Node = get_tree().get_first_node_in_group("player")
 @onready var second_phase: bool = false
+@onready var start_position: Vector2 = global_position
+
 
 func _physics_process(_delta):
-	var player_dist = agro_dist + 1
+	#var player_dist = agro_dist + 1
+	
 	if player != null:
-		player_dist = global_position.distance_to(player.global_position)
-	if player_dist < agro_dist:
-		_target = player
-		agro_dist = 450
+		var player_dist: float = global_position.distance_to(player.global_position)
+		if player_dist <= agro_dist:
+			_target = player
+			agro_dist = 450
+		else:
+			_target = null
 	else:
 		if (player == null):
 			player = get_tree().get_first_node_in_group("player")
 		return
 	
-	var new_target = _target.global_position + _rand_target_mod
+	#var new_target = _target.global_position + _rand_target_mod
+	if (_target != null):
+		motion_controller.acc_dir = global_position.direction_to(_target.global_position)
+	else:
+		motion_controller.acc_dir = global_position.direction_to(start_position)
 	
-	motion_controller.acc_dir = global_position.direction_to(new_target)
 	motion_controller.update(_delta)
 	velocity = motion_controller.get_velocity()
 	move_and_slide()
 	
-func summon_voles():
+func summon_voles(large_ring):
 	var angle: float
 	var giant_vole_scene: PackedScene = preload("res://src/enemies/giant_vole.tscn")
 	var vole_guard: Node
@@ -38,15 +46,27 @@ func summon_voles():
 		vole_guard = giant_vole_scene.instantiate()
 		
 		angle = i * 0.25 * PI
-		vole_guard.global_position = player.global_position + 96 * Vector2(cos(angle), sin(angle))
+		vole_guard.global_position = player.global_position + 128 * Vector2(cos(angle), sin(angle))
 		
 		call_deferred("add_sibling", vole_guard, false)
-
+		
+	if (large_ring == 1):
+		for i in 16:
+			vole_guard = giant_vole_scene.instantiate()
+			
+			angle = i * 0.125 * PI
+			vole_guard.global_position = player.global_position + 192 * Vector2(cos(angle), sin(angle))
+			
+			call_deferred("add_sibling", vole_guard, false)
+		
 func _on_health_container_health_depleted():
 	queue_free()
 
 func _on_hurt_box_hurt(hit_box):
 	motion_controller.apply_impulse((global_position - hit_box.global_position).normalized() * 0.7 * hit_box.knockback)
-	if (second_phase == false && health_checker.get_health() <= health_checker.max_health / 2):
+	if (second_phase == false && health_checker.get_health() <= health_checker.max_health * 0.5): 
 		second_phase = true
-		summon_voles()
+		if (health_checker.get_health() <= health_checker.max_health * 0.3):
+			summon_voles(1)
+		else:
+			summon_voles(0)
