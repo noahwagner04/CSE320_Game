af858b888c6af501e0d959c1c1d8eac3c9fc1444
Lukas-bit <lukasrenschler@gmail.com>
2024-02-10 17:10:11 -0800

    Make Guard Voles Spawn in a Ring Around Player
    Changed the summoned Giant Voles so that they will spawn
    around the player instead of the Behemoth Vole. The summoned Giant Voles
    are now siblings of the Behemoth Vole in the scene tree, instead of
    children.

--- src/enemies/boss_behemoth_vole.gd
+++ src/enemies/boss_behemoth_vole.gd
@@ -8,7 +8,7 @@ var _target: Node2D
 @onready var player: Node = get_tree().get_first_node_in_group("player")
 @onready var motion_controller: MotionController = %MotionController
 @onready var health_checker: HealthContainer = %HealthContainer
-@onready var second_stage: bool = false
+@onready var second_phase: bool = false
 
 func _physics_process(_delta):
 	var player_dist = agro_dist + 1
@@ -30,21 +30,24 @@ func _physics_process(_delta):
 	move_and_slide()
 	
 func summon_voles():
+	var angle: float
 	var giant_vole_scene: PackedScene = preload("res://src/enemies/giant_vole.tscn")
+	var vole_guard: Node
+	
 	for i in 8:
-		var vole_guard = giant_vole_scene.instantiate()
-		var angle: float = i * 0.25 * PI
-		#print("player position is: ", player.global_position)
-		vole_guard.position = 128 * Vector2(cos(angle), sin(angle))
-		#print("vole guard position is: ", vole_guard.position)
-		call_deferred("add_child", vole_guard)
+		vole_guard = giant_vole_scene.instantiate()
+		angle = i * 0.25 * PI
 		
+		vole_guard.global_position =  + player.global_position + 96 * Vector2(cos(angle), sin(angle))
+		#print("vole_guard global position is: ", vole_guard.global_position)
+		#print("vole_guard position is: ", vole_guard.position)
+		call_deferred("add_sibling", vole_guard, false)
 
 func _on_health_container_health_depleted():
 	queue_free()
 
 func _on_hurt_box_hurt(hit_box):
 	motion_controller.apply_impulse((global_position - hit_box.global_position).normalized() * 0.7 * hit_box.knockback)
-	if (second_stage == false && health_checker.get_health() < health_checker.max_health / 2):
-		second_stage = true
+	if (second_phase == false && health_checker.get_health() <= health_checker.max_health / 2):
+		second_phase = true
 		summon_voles()
