d764b91872a3a13dfc0683731757681d2e58ad1d
maxton.greener <maxton.greener@gmail.com>
2024-03-25 14:37:19 -0700

    It's loot table 2: electric boogaloo! Now done through inspector.

---
+++ src/loot_tables/entries/entry.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Entry
+
+#@export var scene: PackedScene
+@export var item: ItemData
+@export var weight: int
---
+++ src/loot_tables/pools/pool.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Pool
+
+@export var rolls: int
+@export_range(0, 100, 0.1, "suffix: %") var chance_to_roll: float = 0
+@export var entries: Array[Entry]
+var total_weight: int
