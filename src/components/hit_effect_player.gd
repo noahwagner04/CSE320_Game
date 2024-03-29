430837937ea42bb94a0491ad7e4e4d4afabc08d4
Noah Wagner <wagnernoah@icloud.com>
2024-03-29 10:55:06 -0700

    export various animation variables

--- src/components/hit_effect_player.gd
+++ src/components/hit_effect_player.gd
@@ -1,6 +1,9 @@
 extends AnimationPlayer
 
 @export var sprite: Sprite2D
+@export var length: float = 0.15
+@export var transition: float = 3
+@export_range(0, 1, 0.01) var compression: float = 0.6
 
 var _flash_material = preload("res://assets/materials/flash_material.tres")
 
@@ -17,16 +20,16 @@ func _ready():
 	
 	var opacity_index = animation.add_track(Animation.TYPE_VALUE)
 	animation.track_set_path(opacity_index, relative_sprite_path + ":material:shader_parameter/opacity")
-	animation.track_insert_key(opacity_index, 0.0, 1, 3)
-	animation.track_insert_key(opacity_index, 0.15, 0)
+	animation.track_insert_key(opacity_index, 0.0, 1, transition)
+	animation.track_insert_key(opacity_index, length, 0)
 	
 	var size_index = animation.add_track(Animation.TYPE_VALUE)
 	animation.track_set_path(size_index, relative_sprite_path + ":scale")
 	var sprite_scale = sprite.scale
-	animation.track_insert_key(size_index, 0.0, Vector2(sprite_scale.x * 0.6, sprite_scale.y), 3)
-	animation.track_insert_key(size_index, 0.15, sprite_scale)
+	animation.track_insert_key(size_index, 0.0, Vector2(sprite_scale.x * compression, sprite_scale.y), transition)
+	animation.track_insert_key(size_index, length, sprite_scale)
 	
-	animation.length = 0.15
+	animation.length = length
 	
 	var animation_library = AnimationLibrary.new()
 	animation_library.add_animation("hit", animation)
