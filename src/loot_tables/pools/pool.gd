4b0ef71d5931b66e1f88f62423c3e80ab5fcba4e
maxton.greener <maxton.greener@gmail.com>
2024-03-30 10:03:50 -0700

    Change "rolls" to "min_rolls" and "max_rolls". Ideally no one messes up and puts invalid numbers but in case they do, min_rolls is respected first (these will go no matter what) and then if max_rolls is less than or equal to min_rolls, it will be treated as equal to min_rolls and stop rolling.

--- src/loot_tables/pools/pool.gd
+++ src/loot_tables/pools/pool.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name Pool
 
-@export var rolls: int
+@export var min_rolls: int
+@export var max_rolls: int
 @export_range(0, 100, 0.1, "suffix: %") var chance_to_roll: float = 0
 @export var entries: Array[Entry]
