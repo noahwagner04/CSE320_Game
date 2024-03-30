a417c95c690d6ce39be8ab386725dcc1e4397b23
Jambr0li <jasonkovalenko@gmail.com>
2024-03-30 06:53:32 -0700

    orbs slowly move towards player, also added level to UI

--- src/components/xp_orb.gd
+++ src/components/xp_orb.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var xp_value: int = 10
+var xp_value: int = 1000
 var xp_direction: Vector2 = Vector2(0.0, 0.0)
 
 @onready var motion_controller = $MotionController
@@ -13,18 +13,14 @@ var sync_pos := Vector2.ZERO
 
 var xp_speed: float = 0
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	home.global_position = global_position
-	motion_controller.max_speed += 20
-	xp_speed = randf_range(10,60)
-	var random_angle = randf_range(0, 2 * PI)
-	xp_direction = Vector2(cos(random_angle), sin(random_angle))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_target = col_detector.get_closest_collider()
-	print(_target)
 	_target = home if _target == null else _target 
 
 	var new_target = _target.global_position + _rand_target_mod
