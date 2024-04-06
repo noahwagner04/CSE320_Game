d3553414cfb6faeb715f9031622b4aaba81d2288
Lukas-bit <lukasrenschler@gmail.com>
2024-04-05 21:42:53 -0700

    Add New Dungeon Level Reachable from Meadow Level
    Added a new map for the player to sent to. This set up in not the best way,
    but it's a working rough draft of a general idea. The second boss is also
    now located here.

--- src/places/meadow.gd
+++ src/places/meadow.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var giant_vole_timer: Timer = $GiantVoleSpawner/Timer
 @onready var giant_dragonfly_timer: Timer = $GiantDragonflySpawner/Timer
 @onready var boss_behemoth_vole_scene: PackedScene = preload("res://src/enemies/boss_behemoth_vole.tscn")
+@onready var meadow_dungeon_scene: PackedScene = preload("res://src/places/meadow_dungeon.tscn")
 
 
 func _ready():
@@ -27,3 +28,13 @@ func _on_area_2d_body_entered(_body):
 	var behemoth_boss: Node = boss_behemoth_vole_scene.instantiate()
 	behemoth_boss.global_position = collision_shape.global_position
 	call_deferred("add_child", behemoth_boss)
+
+
+func _on_meadow_dungeon_area_body_entered(body):
+	var meadow_dungeon_instance: Node = meadow_dungeon_scene.instantiate()
+	call_deferred("add_sibling", meadow_dungeon_instance)
+
+	var player: Node = get_tree().get_first_node_in_group("player")
+	player.global_position = Vector2(205,-409)
+	
+	queue_free()
