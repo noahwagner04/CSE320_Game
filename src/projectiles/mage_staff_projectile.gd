460b4b79669de85f97bd9b5c82c526d4fa39f552
maxton.greener <maxton.greener@gmail.com>
2024-04-12 11:19:22 -0700

    Add call_deferred call to add_child.

--- src/projectiles/mage_staff_projectile.gd
+++ src/projectiles/mage_staff_projectile.gd
@@ -5,8 +5,8 @@ var explosion: PackedScene = preload("res://src/projectiles/explosion.tscn")
 func explode():
 	if aoe_explosion == true:
 		var explosion_instance = explosion.instantiate()
-		add_child(explosion_instance)
-		explosion_instance.projectile_damage = 3 * projectile_damage
+		call_deferred("add_child", explosion_instance)
+		explosion_instance.set_deferred("projectile_damage", 3 * projectile_damage)
 		
 func _on_hit_box_area_entered(_area):
 	explode()
